b96252fa1d8f2fba0253f634aceb696f1e6b8c33
Klaster28 <89096914521@mail.ru>
2022-10-01 22:31:04 +0300

    Коммит после первого задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+     int clientOs = 0;
+     if (clientOs == 1) {
+         System.out.println("Установите версию приложения для Android по ссылке ... ");
+     } else {
+         System.out.println("Установите версию приложения для IOS по ссылке...");
+     }
+
+
 
 
     }
