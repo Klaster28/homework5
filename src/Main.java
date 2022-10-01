97e1e6d84cf352811134d017e656e034aede75be
Klaster28 <89096914521@mail.ru>
2022-10-01 22:38:24 +0300

    Коммит после правки первого задания

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-     int clientOs = 0;
+     int clientOs = 4;
      if (clientOs == 1) {
          System.out.println("Установите версию приложения для Android по ссылке ... ");
-     } else {
+     } else if (clientOs == 0){
          System.out.println("Установите версию приложения для IOS по ссылке...");
+     } else {
+         System.out.println("Неверное введены данные Операционной системы!");
      }
+
+
 
 
 
